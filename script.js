143e000cb02659577a83dde083ccbdd70509a15c
Darion Valdez <Darionvldz@gmail.com>
2024-02-13 12:26:45 +0100

    make player able to add tile in console gameboard with prompt

--- script.js
+++ script.js
@@ -1,58 +1,87 @@
-function game(){
+const gameBoard = (function(){
+    let gameBoardArray = [["","",""],["","",""],["","",""]];
 
-    let numberOfTurns = 0;
-
-    const getNumberOfTurns = () => numberOfTurns;
-
-    const increaseNumberOfTurns = () => numberOfTurns ++;
-
-    const newGame = function() {
-
-    }
-
-    const nexTurn = function() {
-
-    }
-
-    const makeComputerMove = function() {
-
-    }
-
-    const win = function() {
-
-    }
-
-    return { getNumberOfTurns, increaseNumberOfTurns, newGame, nexTurn, makeComputerMove, win }
-}
-
-function gameBoard(){
-    let gameBoard = [[],[],[]];
+    const getGameBoard = () => gameBoardArray;
 
     const resetBoard = function() {
 
     }
 
-    const isMoveLegal = function(){
-
+    const isMoveLegal = function(row, column){
+        return gameBoardArray[row][column] === '';
     }
 
-    const addMove = function(token){
-
+    const makeMove = function(token, row, column){
+        gameBoardArray[row][column] = token;
     }
 
     const validateMove = function(){
-
+        
     }
 
     const showWinningTiles = function(){
 
     }
 
-    return { resetBoard, isMoveLegal, addMove, validateMove, showWinningTiles }
-}
+    return { getGameBoard, resetBoard, isMoveLegal, makeMove, validateMove, showWinningTiles }
+})();
 
-function player(token){
-    let token = token;
+const gameLoop = (function(){
+
+    let numberOfTurns = 0;
+    let whoseTurn = 1;
+    let player = {};
+
+    const getNumberOfTurns = () => numberOfTurns;
+
+    const increaseNumberOfTurns = () => numberOfTurns ++;
+
+    const newGame = function() {
+        player = createPlayer('X');
+        computer = createPlayer('O');
+    }
+
+    const nextTurn = function() {
+        if(whoseTurn) { 
+            whoseTurn = 0;
+            makePlayerMove();
+        } else {
+            whoseTurn = 1;
+            makeComputerMove();
+        }
+    }
+
+    const makeComputerMove = function() {
+        console.log('I dont have a brain yet');
+    }
+
+    const makePlayerMove = function() {
+        let playerMoveRow = prompt('Row?') - 1;
+        let playerMoveColumn = prompt('Column?') - 1;
+
+        if(gameBoard.isMoveLegal(playerMoveRow, playerMoveColumn)){
+            gameBoard.makeMove(player.getToken(), playerMoveRow, playerMoveColumn);
+        }
+
+        updateBoard();
+        nextTurn();
+    }
+
+    const updateBoard = function(){
+        console.table(gameBoard.getGameBoard());
+    }
+
+    const win = function() {
+
+    }
+    
+    newGame();
+    nextTurn();
+
+})();
+
+function createPlayer(token){
+
     const getToken = () => token;
 
     return { getToken }
